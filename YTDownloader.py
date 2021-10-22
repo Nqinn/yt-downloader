2bbb48d41f3b0185030df387b5c336b2a9f8b085
Qi <76272025+nqii@users.noreply.github.com>
2021-10-22 20:20:49 +0700

    Add files via upload

--- YTDownloader.py
+++ YTDownloader.py
@@ -1,16 +1,26 @@
 from pytube import YouTube
-link = input("enter your youtube url : ")
-yt = YouTube(link)
-videos = yt.streams.all()
 
+# Asking user to insert the link
+link = input("Enter the youtube url: ")
+yt = YouTube(link)
+
+#Show the video details
+print("Title: ",yt.title)
+print("Number of views: ",yt.views)
+print("Length of video: ",yt.length,"seconds")
+print("Ratings: ",yt.rating)
+#Showing MP4 options only
+videos = yt.streams.filter(progressive=True)
 video  =list(enumerate(videos))
 
 for i in video:
     print(i)
 
-print("enter the desired option to download the format")
-dn_option = int(input("enter the option : "))
+print("Enter the desired options to download the format!")
+dn_option = int(input("Enter the option: "))
 dn_video = videos[dn_option]
 dn_video.download()
 
-print ("downloaded successfully")
+#Starting Download
+print ("Downloading...")
+print("Your video has been downloaded :D")
